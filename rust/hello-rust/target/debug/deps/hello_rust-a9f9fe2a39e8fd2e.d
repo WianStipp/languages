f53387bb637b1f64e8a60b661b5b0e4080d28552
Wian Stipp <stippwian@gmail.com>
2023-03-05 23:21:18 -0800

    added rust

---
+++ rust/hello-rust/target/debug/deps/hello_rust-a9f9fe2a39e8fd2e.d
@@ -0,0 +1,5 @@
+/Users/wianstipp/Desktop/projects/languages/rust/hello-rust/target/debug/deps/hello_rust-a9f9fe2a39e8fd2e: src/main.rs
+
+/Users/wianstipp/Desktop/projects/languages/rust/hello-rust/target/debug/deps/hello_rust-a9f9fe2a39e8fd2e.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/wianstipp/Desktop/projects/languages/rust/hello-rust/target/debug/hello-rust: /Users/wianstipp/Desktop/projects/languages/rust/hello-rust/src/main.rs
